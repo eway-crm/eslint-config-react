18d942e7c8a7b48435f8fc3bd433e3d2ec078039
Štěpán Havránek <stepan.havranek@eway-crm.com>
2023-09-08 14:22:59 +0200

    refs #44875: Ingerit react-hooks plugin, Make both rules error causing

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 module.exports = {
     "plugins": [
-        "react"
+        "react",
+        "react-hooks"
     ],
     "extends": [
         "@eway-crm/eslint-config",
     ],
     "rules": {
-        "react/self-closing-comp": "error"
-      }
+        "react/self-closing-comp": "error",
+        "react-hooks/rules-of-hooks": "error",
+        "react-hooks/exhaustive-deps": "error"
+    }
 };
